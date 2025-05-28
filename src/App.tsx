1aa5e8bd48a2a7769c76f5e9c0749a2ad3d7bfe0
Omar frihi <o.frihi@externe.labelvie.ma>
2025-05-28 19:00:56 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [animationReady, setAnimationReady] = useState(false);
+  const [isSignIn, setIsSignIn] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSignIn(true);
+      setAnimationReady(true); // now animations will run
+    }, 100);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const toggle = () => {
+    setIsSignIn((prev) => !prev);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id="container"
+      className={`container ${
+        animationReady ? (isSignIn ? "sign-in" : "sign-up") : ""
+      }`}
+    >
+      <div className="row">
+        {/* SIGN UP */}
+        <div className="col align-items-center flex-col sign-up">
+          <div className="form-wrapper align-items-center">
+            <div className="form sign-up">
+              <div className="input-group">
+                <i className="bx bxs-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-group">
+                <i className="bx bx-mail-send"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-group">
+                <i className="bx bxs-lock-alt"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <div className="input-group">
+                <i className="bx bxs-lock-alt"></i>
+                <input type="password" placeholder="Confirm password" />
+              </div>
+              <button>Sign up</button>
+              <p>
+                <span>Already have an account?</span>
+                <b onClick={toggle} className="pointer">
+                  Sign in here
+                </b>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* SIGN IN */}
+        <div className="col align-items-center flex-col sign-in">
+          <div className="form-wrapper align-items-center">
+            <div className="form sign-in">
+              <div className="input-group">
+                <i className="bx bxs-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-group">
+                <i className="bx bxs-lock-alt"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <button>Sign in</button>
+              <p>
+                <b>Forgot password?</b>
+              </p>
+              <p>
+                <span>Don't have an account?</span>
+                <b onClick={toggle} className="pointer">
+                  Sign up here
+                </b>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* CONTENT SECTION */}
+      <div className="row content-row">
+        <div className="col align-items-center flex-col">
+          <div className="text sign-in">
+            <h2>Welcome</h2>
+          </div>
+          <div className="img sign-in"></div>
+        </div>
+        <div className="col align-items-center flex-col">
+          <div className="img sign-up"></div>
+          <div className="text sign-up">
+            <h2>Join with us</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
